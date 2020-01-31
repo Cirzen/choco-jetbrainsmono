a13386d906613f805ca0c1b490d74126d7a35ae3
David Johnson <david.johnson@crispthinking.com>
2020-01-31 12:53:53 +0000

    Move to font helper module dependency.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,36 +4,27 @@
   [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
 }
 
-$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
-. $fontHelpersPath
-$packageName = "JetBrainsMono"
+$ExitCode = 0
+$PackageName = "JetBrainsMono"
 
-$fontUrl = 'https://download.jetbrains.com/fonts/JetBrainsMono-1.0.0.zip'
-$checksumType = 'sha256';
-$checksum = '82BF0DEC956E4CA9AFA4DA4978FA5DE80A75A576B3353D570CCBAABE4E858389';
+$FontUrl = 'https://download.jetbrains.com/fonts/JetBrainsMono-1.0.2.zip'
+$ChecksumType = 'sha256';
+$Checksum = '59F9B9762D5625EB438EEDF034DBBCDCF09ED18DED994540B466872840229762';
 
-$tempInstallPath = Join-Path $Env:Temp $packageName
+$TempInstallPath = Join-Path $Env:Temp $PackageName
 
-Install-ChocolateyZipPackage -PackageName $packageName -url $fontUrl -unzipLocation $tempInstallPath -ChecksumType $checksumType -Checksum $checksum
+Install-ChocolateyZipPackage -PackageName $PackageName -url $FontUrl -unzipLocation $TempInstallPath -ChecksumType $ChecksumType -Checksum $Checksum
 
-$shell = New-Object -ComObject Shell.Application
-$fontsFolder = $shell.Namespace(0x14)
+$FontFiles = Get-ChildItem $TempInstallPath -Recurse -Filter *.ttf -File
+$FontCount = @($FontFiles).Count
 
-$fontFiles = Get-ChildItem $tempInstallPath -Recurse -Filter *.ttf
-
-# unfortunately the font install process totally ignores shell flags :(
-# http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
-# so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
-$commands = $fontFiles |
-% { Join-Path $fontsFolder.Self.Path $_.Name } |
-? { Test-Path $_ } |
-% { "Remove-SingleFont '$_' -Force;" }
-
-# http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
-$fontFiles |
-% { $commands += "Add-SingleFont '$($_.FullName)';" }
-
-$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
-Start-ChocolateyProcessAsAdmin $toExecute
+$SuccessCount = Install-ChocolateyFont -Paths @($FontFiles.FullName) -Multiple
+if ($SuccessCount -ne $FontCount)
+{
+  Write-Error ("Failed installing all fonts: {0} Success / {1} Total" -f $SuccessCount, $FontCount)
+  $ExitCode = 1
+}
 
 Remove-Item $tempInstallPath -Recurse
+
+exit $ExitCode
